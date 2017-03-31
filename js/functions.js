3f8642981a75e7edf721be54f19e1bc8481c8102
amberMZ <amberzhangx@gmail.com>
2017-03-30 20:44:02 -0400

    related ingredients/concerns

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,32 @@
 
+var concerns_list = ['acne', 'dry', 'oily', 'dull', 'dark-spot', 'aging'];
+var goals_list = ['balancing', 'hydrating', 'oilControl', 'brightening', 'colorCorrecting', 'anti-aging'];
 
-var concerns = ['Acne', 'Dry', 'Dull', 'Anti-Aging', 'Dark Circles']
-var concernToIngredient = new map();
-map.set(concerns[0], ['Vitamin C', 'Retinol', 'Zinc', 'Salicylic acid', 'Witch hazel', 'Licorice'])
-map.set(concerns[1], ['Vitamin E', 'Niacinamide', 'Glycolic Acid', 'Hyaluronic Acid', 'Tahitian Black Pearl Essence'])
-map.set(concerns[2], ['Olive Extract', 'Resveratrol'])
+var ingredient = new Map();
+ingredient.set(0,['Tea tree', 'Lavender']);
+ingredient.set(1,['Sandalwood', 'Myrrh']);
+ingredient.set(2,['Orange', 'Bergamot']);
+ingredient.set(3,['Lemon', 'Bergamot']);
+ingredient.set(4,['Lemon', 'Frankincense']);
+ingredient.set(5,['Tangerine', 'Frankincense']);
+
+var related_concerns = new Map();
+related_concerns.set(0, [2, 4]);
+related_concerns.set(1, [3, 5]);
+related_concerns.set(2, [0, 3]);
+related_concerns.set(3, [4, 5]);
+related_concerns.set(4, [1, 5]);
+related_concerns.set(5, [1, 3]);
+
+// related_ingredient.set(concerns[5],['Tangerine', 'Frankincense']);related_ingredient.set(concerns[0],['Tea tree', 'Lavender']);
+// related_ingredient.set(concerns[1],['Sandalwood', 'Myrrh']);
+// related_ingredient.set(concerns[2],['Orange', 'Bergamot']);
+// related_ingredient.set(concerns[3],['Lemon', 'Bergamot']);
+// related_ingredient.set(concerns[4],['Lemon', 'Frankincense']);
+// related_ingredient.set(concerns[5],['Tangerine', 'Frankincense']);
+
+// related_ingredient.set(concerns[1], ['Vitamin E', 'Niacinamide', 'Glycolic Acid', 'Hyaluronic Acid', 'Tahitian Black Pearl Essence'])
+// related_ingredient.set(concerns[2], ['Olive Extract', 'Resveratrol'])
 
 
 
@@ -51,7 +73,7 @@ function updateGoals(goals){
 	document.getElementById(goals).setAttribute("class",'selected');	
 	localStorage.setItem("goals", goals);
     
-	if (goals!="balancing"){
+	if (goals!=goals_list[0]){
 		document.getElementById('balancing').setAttribute("class",'unselected');
 	}
 	if (goals!="hydrating"){
